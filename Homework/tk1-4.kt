eac53fcce0262da3cc011e193ab614731c397859
MaximArtemev <maksim-makim@yandex.ru>
2017-02-12 12:41:13 +0300

    Delete tk1-4.kt

--- Homework/tk1-4.kt
+++
@@ -1,9 +0,0 @@
-import java.util.*
-
-fun main(args: Array<String>) {
-    var count = args[0].toInt()
-    var a = IntArray(10) {Random().nextInt(10)}.asList()
-    a = a.sorted()
-    println(a)
-    println(a[a.count()/2])
-}
